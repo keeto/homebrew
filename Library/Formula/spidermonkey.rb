7f005486f47e9aa37d5b67daa39bd56b13adfd8c
Keeto Obcena <markeeto@gmail.com>
2010-08-05 22:42:26 +0800

    Spidermonkey 1.9.2

--- Library/Formula/spidermonkey.rb
+++ Library/Formula/spidermonkey.rb
@@ -1,21 +1,19 @@
 require 'formula'
 
-class Spidermonkey <Formula
-  # There are no proper releases of spidermonkey, so pick a version that's known
-  # to work (especially with CouchDB), revision r35345.
+class Spidermonkey <Formula  
+  # There are no proper releases of spidermonkey. So pick a specific/constant
+  # revision:  r35345
   url 'http://hg.mozilla.org/tracemonkey/archive/57a6ad20eae9.tar.gz'
-  homepage 'https://developer.mozilla.org/en/SpiderMonkey'
   md5 '2d8cf22da82b30c36f47675a8486a3f3'
-  version '1.8.5'
+
+  version '1.9.2'
+
+  homepage 'https://developer.mozilla.org/en/SpiderMonkey'
+
+  head 'hg://http://hg.mozilla.org/tracemonkey'
 
   depends_on 'readline'
   depends_on 'nspr'
-
-  def patches
-    # Export date functions needed by manually-compiled MongoDB.
-    # Is it just me or is the version-to-version stable API of SpiderMonkey kind of a mess?
-    "http://gist.github.com/raw/426476/a98a15a94ca4efd3aeafb3b5cd943491b53cbf81/001-Properly-export-js_DateClass-and-js_RegExpClass.patch"
-  end
 
   def install
     if MACOS_VERSION == 10.5
@@ -26,12 +24,12 @@ class Spidermonkey <Formula
     end
 
     # For some reason SpiderMonkey requires Autoconf-2.13
-    ac213_prefix = Pathname.pwd.join('ac213')
+    ac213_prefix = Pathname.pwd.join('ac213').to_s
     Autoconf213.new.brew do |f|
       # probably no longer required, see issue #751
       inreplace 'configure', 'for ac_prog in mawk gawk nawk awk', 'for ac_prog in awk'
 
-      system "./configure", "--disable-debug",
+      system "./configure", "--disable-debug", 
                             "--program-suffix=213",
                             "--prefix=#{ac213_prefix}"
       system "make install"
@@ -46,7 +44,7 @@ class Spidermonkey <Formula
       inreplace "config/rules.mk", "-install_name @executable_path/$(SHARED_LIBRARY) ", ""
     end
 
-    FileUtils.mkdir "brew-build"
+    FileUtils.mkdir "brew-build";
 
     Dir.chdir "brew-build" do
       system "../js/src/configure", "--prefix=#{prefix}",
@@ -55,6 +53,7 @@ class Spidermonkey <Formula
                                     "--with-system-nspr"
 
       inreplace "js-config", /JS_CONFIG_LIBS=.*?$/, "JS_CONFIG_LIBS=''"
+
       # Can't do `make install` right off the bat sadly
       system "make"
       system "make install"
@@ -62,6 +61,7 @@ class Spidermonkey <Formula
       # The `js` binary ins't installed. Lets do that too, eh?
       bin.install "shell/js"
     end
+
   end
 end
 
@@ -71,3 +71,5 @@ class Autoconf213 <Formula
   md5 '9de56d4a161a723228220b0f425dc711'
   homepage 'http://www.gnu.org/software/autoconf/'
 end
+
+
